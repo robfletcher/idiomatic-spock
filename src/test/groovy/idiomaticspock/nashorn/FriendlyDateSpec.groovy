dd83f0c9502e262a30bac71cac5d559ced6a7a25
Rob Fletcher <rfletcher@netflix.com>
2014-05-31 14:09:21 +0100

    nashorn test

---
+++ src/test/groovy/idiomaticspock/nashorn/FriendlyDateSpec.groovy
@@ -0,0 +1,36 @@
+package idiomaticspock.nashorn
+
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Subject
+import spock.lang.Unroll
+
+import javax.script.ScriptEngineManager
+
+import static java.time.LocalDateTime.now
+
+class FriendlyDateSpec extends Specification {
+  @Shared def engine = new ScriptEngineManager().getEngineByName("nashorn")
+  @Shared @Subject def moment
+
+  def setupSpec() {
+    getClass().getResourceAsStream("/moment-with-langs.js").withReader { reader ->
+      engine.eval reader
+    }
+
+    moment = engine.invokeFunction("moment")
+  }
+
+  @Unroll
+  def "The date #date in friendly format is #expectedResult"() {
+    expect:
+    engine.invokeMethod(moment, "from", date.toString()) == expectedResult
+
+    where:
+    date                 | expectedResult
+    now().plusDays(2)    | "2 days ago"
+    now().plusMinutes(2) | "2 minutes ago"
+    now()                | "a few seconds ago"
+    now().minusDays(1)   | "in a day"
+  }
+}
